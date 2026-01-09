03c6c929a3a76368ec5919b12dde4faf120c47c0
unknown <anwar.saeed@gmail.com>
2026-01-09 09:43:14 +0600

    my portfolio update

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,8 @@
 <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-gradient-to-r from-gray-700 to-gray-800 text-white">
     <x-app-logo-icon class="size-5 fill-current text-white" />
 </div>
+@if(!Route::is('login') && !Route::is('register') && !url('forgot-password') && !url('reset-password'))
 <div class="ms-1 grid flex-1 text-start text-sm">
     <span class="mb-0.5 truncate leading-tight font-semibold">My Portfolio</span>
 </div>
+@endif
--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -7,11 +7,7 @@
     <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div class="flex w-full max-w-sm flex-col gap-2">
             <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium">
-                <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md bg-gradient-to-r from-blue-500 to-purple-600">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
-                    </svg>
-                </span>
+                <x-app-logo />
                 <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
             </a>
             <div class="flex flex-col gap-6">
